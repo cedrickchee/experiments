d370d5a6ad7e6a0ead63c5bfbed442d96c793dd9
Cedric Chee <cedric@invictusbyte.com>
2019-04-29 23:21:13 +0800

    GatsbyJS basic starter tutorial

---
+++ reactjs/gatsbyjs/tutorial/gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
---
+++ reactjs/gatsbyjs/tutorial/src/pages/allo-werl-again.md
@@ -0,0 +1,11 @@
+---
+title: "Allo Werl Again"
+date: "2019-02-19"
+---
+
+Hello world, my friends.
+
+## This is Fun!
+
+- Moar fun
+- Cool
