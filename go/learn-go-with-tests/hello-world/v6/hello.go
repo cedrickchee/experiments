4ae610abb4cd176cafa80ba57a83ee1c83b4d4c1
Cedric Chee <cedric@invictusbyte.com>
2019-10-02 12:57:23 +0000

    Refactor greeting program

---
+++ go/learn-go-with-tests/hello-world/v6/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+// Hello returns a personalised greeting in a given language
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
