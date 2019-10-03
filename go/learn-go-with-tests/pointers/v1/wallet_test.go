32a1cb0000554e25d46942675942487689097707
Cedric Chee <cedric@invictusbyte.com>
2019-10-03 16:56:03 +0000

    Bitcoin wallet

---
+++ go/learn-go-with-tests/pointers/v1/wallet_test.go
@@ -0,0 +1,19 @@
+package bitcoin
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
