4ea9fd2fad69562a58e615c346c808219aac3604
Cedric Chee <cedric@invictusbyte.com>
2019-10-03 15:46:25 +0000

    Further refactor: table driven tests

---
+++ go/learn-go-with-tests/structs/v5/shapes_test.go
@@ -0,0 +1,31 @@
+package geometry
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{50.0, 50.0}
+	got := Perimeter(rectangle)
+	want := 200.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 5.0}, 50.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
