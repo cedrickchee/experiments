20e9493302a5b1ab8c17139e8fcbd3ab3c3c7339
Cedric Chee <cedric@invictusbyte.com>
2019-10-03 18:21:30 +0000

    Implement dictionary using map

---
+++ go/learn-go-with-tests/maps/v1/dictionary_test.go
@@ -0,0 +1,20 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	got := dictionary.Search("test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
