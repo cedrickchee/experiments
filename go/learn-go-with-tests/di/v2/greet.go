50526e405ef638c5d96a6b8ef9304440cff74ae0
Cedric Chee <cedric@invictusbyte.com>
2019-10-14 06:03:32 +0000

    Write data to HTTP server using io.Writer

---
+++ go/learn-go-with-tests/di/v2/greet.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Greet sends a personalised greeting to writer
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+// MyGreeterHandler says Hello, world over HTTP
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "John")
+
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+}
