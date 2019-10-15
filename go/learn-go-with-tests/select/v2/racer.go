0e28fae91f64b1cf118dd33632b7454623a46401
Cedric Chee <cedric@invictusbyte.com>
2019-10-15 14:26:14 +0000

    Refactor: simpler solution using select

---
+++ go/learn-go-with-tests/select/v2/racer.go
@@ -0,0 +1,25 @@
+package racer
+
+import (
+	"net/http"
+)
+
+// Racer compares the response times of a and b, returning the fastest one
+func Racer(a, b string) (winner string) {
+	// What select lets you do is wait on multiple channels
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
+}
+
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
+}
