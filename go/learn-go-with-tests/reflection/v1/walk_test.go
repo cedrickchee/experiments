b31d2a345b4dd4bfbbe9714b153dbe8dfa496832
Cedric Chee <cedric@invictusbyte.com>
2019-10-15 15:40:58 +0000

    Walk struct with one string field

---
+++ go/learn-go-with-tests/reflection/v1/walk_test.go
@@ -0,0 +1,36 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"John"},
+			[]string{"John"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
