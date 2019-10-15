85d2ae7fa2431818e7ccb8c796887e4e28045dde
Cedric Chee <cedric@invictusbyte.com>
2019-10-15 16:05:28 +0000

    Walk nested fields

---
+++ go/learn-go-with-tests/reflection/v4/walk.go
@@ -0,0 +1,18 @@
+package reflection
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
