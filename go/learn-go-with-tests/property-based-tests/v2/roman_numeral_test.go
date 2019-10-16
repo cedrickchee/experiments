246519c318a0bc81b29177b89f41eb801df3292f
Cedric Chee <cedric@invictusbyte.com>
2019-10-16 12:51:07 +0000

    Refactor: use table based tests

--- go/learn-go-with-tests/property-based-tests/v2/roman_numeral_test.go
+++ go/learn-go-with-tests/property-based-tests/v2/roman_numeral_test.go
@@ -3,21 +3,23 @@ package romannumeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Roman       string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			want := test.Roman
 
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
+			if got != want {
+				t.Errorf("got %q want %q", got, want)
+			}
+		})
+	}
 }
