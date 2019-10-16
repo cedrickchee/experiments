1b9555cd75cfc6ab89060a62d6e8fd57431f720c
Cedric Chee <cedric@invictusbyte.com>
2019-10-16 14:36:29 +0000

    Final test suite

---
+++ go/learn-go-with-tests/property-based-tests/v9/roman_numeral.go
@@ -0,0 +1,39 @@
+package romannumeral
+
+import "strings"
+
+// RomanNumeral represents Arabic number and its Roman Numeral symbol
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var romanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ConvertToRoman converts an Arabic number (numbers 0 to 9) to a Roman Numeral
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range romanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
---
+++ go/learn-go-with-tests/property-based-tests/v9/roman_numeral_test.go
@@ -0,0 +1,55 @@
+package romannumeral
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Arabic int
+		Roman  string
+	}{
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 13, Roman: "XIII"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 15, Roman: "XV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+
+			if got != test.Roman {
+				t.Errorf("got %q want %q", got, test.Roman)
+			}
+		})
+	}
+}
