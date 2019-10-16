a965512b1067ccb39ba4352115b5452b053facbe
Cedric Chee <cedric@invictusbyte.com>
2019-10-16 13:45:30 +0000

    Refactor: new looping algorithm

---
+++ go/learn-go-with-tests/property-based-tests/v5/roman_numeral.go
@@ -0,0 +1,24 @@
+package romannumeral
+
+import "strings"
+
+// ConvertToRoman converts an Arabic number (numbers 0 to 9) to a Roman Numeral
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+	}
+
+	return result.String()
+}
