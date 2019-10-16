e64189df9fcabbb71fa3c2dea68ae6f59884e0f0
Cedric Chee <cedric@invictusbyte.com>
2019-10-16 14:22:46 +0000

    Test cases for bigger numbers

---
+++ go/learn-go-with-tests/property-based-tests/v8/roman_numeral.go
@@ -0,0 +1,33 @@
+package romannumeral
+
+import "strings"
+
+// RomanNumeral represents Arabic number and its Roman Numeral symbol
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var romanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ConvertToRoman converts an Arabic number (numbers 0 to 9) to a Roman Numeral
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range romanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
