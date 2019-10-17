43de3ca33de53acb6720a4bc73057d1dacb3f030
Cedric Chee <cedric@invictusbyte.com>
2019-10-17 10:47:53 +0000

    Refactor: make 'magic' numbers meaning explicit

---
+++ go/learn-go-with-tests/math/v12/clockface.go
@@ -0,0 +1,56 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+const (
+	secondsInHalfClock = 30
+	secondsInClock     = 2 * secondsInHalfClock
+	minutesInHalfClock = 30
+	minutesInClock     = 2 * minutesInHalfClock
+	hoursInHalfClock   = 6
+	hoursInClock       = 2 * hoursInHalfClock
+)
+
+// A Point represents a two dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return float64(t.Second()) / secondsInHalfClock * math.Pi // or (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	// measure the angle from 12 o'clock rather than from the X axis (3 o'clock),
+	// we need to swap the axis around; now x = sin(a) and y = cos(a).
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / minutesInClock) +
+		(float64(t.Minute()) / minutesInHalfClock * math.Pi)
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / hoursInClock) +
+		(math.Pi / (hoursInHalfClock / (float64(t.Hour() % hoursInClock))))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
