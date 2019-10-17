7103930619ab55bd28919f454fafface1b3c54c3
Cedric Chee <cedric@invictusbyte.com>
2019-10-17 03:18:41 +0000

    Test seconds in radians

---
+++ go/learn-go-with-tests/math/v2/clockface_test.go
@@ -0,0 +1,40 @@
+package clockface
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := Point{X: 150, Y: 150 + 90}
+// 	got := SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
+
+func TestSecondHandInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
+	}
+}
