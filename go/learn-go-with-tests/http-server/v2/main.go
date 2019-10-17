8d4fe8b9b173cf56b0fb1877c529a2877f014068
Cedric Chee <cedric@invictusbyte.com>
2019-10-17 17:10:56 +0000

    Refactor using interfaces

---
+++ go/learn-go-with-tests/http-server/v2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// InMemoryPlayerStore ...
+type InMemoryPlayerStore struct{}
+
+// GetPlayerScore ...
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
