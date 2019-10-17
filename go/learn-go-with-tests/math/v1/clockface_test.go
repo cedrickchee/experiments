6d9ec5d84ad612aa644f117d029360e83fff09e9
Cedric Chee <cedric@invictusbyte.com>
2019-10-17 02:26:49 +0000

    Make an analogue clock

---
+++ go/learn-go-with-tests/math/v1/clockface_test.go
@@ -0,0 +1,17 @@
+package clockface
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
