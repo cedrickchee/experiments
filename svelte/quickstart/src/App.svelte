805bcb5aa4798ef78d6449df0e63ff4abdfa0ae3
Cedric Chee <cedric@invictusbyte.com>
2020-01-01 23:17:19 +0800

    Svelte tutorial - reactivity statements

--- svelte/quickstart/src/App.svelte
+++ svelte/quickstart/src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   let count = 0;
-  $: doubled = count * 2;
+
+  $: if (count >= 10) {
+    alert(`count is dangerously high!`);
+    count = 9;
+  }
 
   function handleClick() {
     // event handler code goes here
@@ -11,4 +15,3 @@
 <button on:click="{handleClick}">
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
-<p>{count} doubled is {doubled}</p>
