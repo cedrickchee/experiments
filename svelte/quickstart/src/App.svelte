94275166a7a10a9350eeaf4bb3c68957b3dae54f
Cedric Chee <cedric@invictusbyte.com>
2020-01-01 22:55:49 +0800

    Svelte tutorial - assignments

--- svelte/quickstart/src/App.svelte
+++ svelte/quickstart/src/App.svelte
@@ -1,49 +1,12 @@
 <script>
-	import Nested from './Nested.svelte';
+  let count = 0;
 
-	export let name;
-
-	let greet = 'Happy New Year!';
-	let year = 2020;
-	let src = 'tutorial/image.gif';
-	let string = 'this string contains some <strong>HTML!!!</strong>';
+  function handleClick() {
+    // event handler code goes here
+    count += 1;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>{greet.toUpperCase()} Have a great {year}.</p>
-	<img {src} alt="{name} dances"/>
-	<span>A foobar span</span>
-
-	<Nested/>
-
-	<p>{@html string}</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	span {
-		color: purple;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<button on:click="{handleClick}">
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
