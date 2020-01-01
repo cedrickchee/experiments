84787bf5073bbd25441c9e1a291d428280c2353f
Cedric Chee <cedric@invictusbyte.com>
2020-01-01 22:48:39 +0800

    Svelte tutorial - making an app

--- svelte/quickstart/src/main.js
+++ svelte/quickstart/src/main.js
@@ -1,10 +1,11 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  // we'll learn about props later
+  props: {
+    name: "world"
+  }
 });
 
 export default app;
