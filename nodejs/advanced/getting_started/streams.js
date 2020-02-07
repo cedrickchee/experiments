1b926bc9c2a4bdb9487460348a1d834435e284cb
Cedric Chee <cedric@invictusbyte.com>
2020-02-07 22:20:42 +0800

    Flow-chart on lifecycle of Node Streams

--- nodejs/advanced/getting_started/streams.js
+++ nodejs/advanced/getting_started/streams.js
@@ -174,3 +174,60 @@ readableStream2.on("readable", () => {
 //
 // Use the end() method:
 writableStream2.end();
+
+/**
+ * How `pipe` really works: readable source will be paused if the queue for the
+ * writable/transform/duplex destination stream is full. Otherwise, the readable
+ * will be resumed and read.
+ * Source: https://nodejs.org/en/docs/guides/backpressuring-in-streams
+ */
+
+/**
+ * Lifecycle of `.pipe()`
+ *
+ * To achieve a better understanding of backpressure, here is a flow-chart on
+ * the lifecycle of a `Readable` stream being piped into a `Writable` stream:
+
+                                                     +===================+
+                         x-->  Piping functions   +-->   src.pipe(dest)  |
+                         x     are set up during     |===================|
+                         x     the .pipe method.     |  Event callbacks  |
+  +===============+      x                           |-------------------|
+  |   Your Data   |      x     They exist outside    | .on('close', cb)  |
+  +=======+=======+      x     the data flow, but    | .on('data', cb)   |
+          |              x     importantly attach    | .on('drain', cb)  |
+          |              x     events, and their     | .on('unpipe', cb) |
++---------v---------+    x     respective callbacks. | .on('error', cb)  |
+|  Readable Stream  +----+                           | .on('finish', cb) |
++-^-------^-------^-+    |                           | .on('end', cb)    |
+  ^       |       ^      |                           +-------------------+
+  |       |       |      |
+  |       ^       |      |
+  ^       ^       ^      |    +-------------------+         +=================+
+  ^       |       ^      +---->  Writable Stream  +--------->  .write(chunk)  |
+  |       |       |           +-------------------+         +=======+=========+
+  |       |       |                                                 |
+  |       ^       |                              +------------------v---------+
+  ^       |       +-> if (!chunk)                |    Is this chunk too big?  |
+  ^       |       |     emit .end();             |    Is the queue busy?      |
+  |       |       +-> else                       +-------+----------------+---+
+  |       ^       |     emit .write();                   |                |
+  |       ^       ^                                   +--v---+        +---v---+
+  |       |       ^-----------------------------------<  No  |        |  Yes  |
+  ^       |                                           +------+        +---v---+
+  ^       |                                                               |
+  |       ^               emit .pause();          +=================+     |
+  |       ^---------------^-----------------------+  return false;  <-----+---+
+  |                                               +=================+         |
+  |                                                                           |
+  ^            when queue is empty     +============+                         |
+  ^------------^-----------------------<  Buffering |                         |
+               |                       |============|                         |
+               +> emit .drain();       |  ^Buffer^  |                         |
+               +> emit .resume();      +------------+                         |
+                                       |  ^Buffer^  |                         |
+                                       +------------+   add chunk to queue    |
+                                       |            <---^---------------------<
+                                       +============+
+
+*/
