b4de3c1635ecedf49c6893dbb018952ef1fd77f5
Cedric Chee <cedric@invictusbyte.com>
2020-02-21 00:35:23 +0800

    Echo routing, path & query parameters examples

--- go/echo-framework/server.go
+++ go/echo-framework/server.go
@@ -17,11 +17,26 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.String(http.StatusOK, "Hello, World")
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
