65edca8c8495b66e0b124b96dbb4e389d44d2b8f
Cedric Chee <cedric@invictusbyte.com>
2020-02-22 20:16:06 +0800

    Recipe for graceful shutdown Echo server

---
+++ go/echo-framework/cookbook/graceful-shutdown/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	// Setup
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	e.GET("/", func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	// Start server
+	go func() {
+		if err := e.Start(":3000"); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a
+	// timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// echo.Shutdown stops the server gracefully.
+	// It internally calls `http.Server#Shutdown()`.
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
