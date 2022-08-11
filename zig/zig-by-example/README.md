3e528351b4f2070432ba9868df13ea05cfcf37ab
Cedric Chee <cedric@invictusbyte.com>
2022-08-11 19:04:46 +0800

    Zig by example; initial commit

---
+++ zig/zig-by-example/README.md
@@ -0,0 +1,6 @@
+# Zig By Example
+
+Learn Zig by simple and small projects.
+
+Website: https://zig-by-example.github.io/
+
---
+++ zig/zig-by-example/ls.zig
@@ -0,0 +1,19 @@
+// 1 - List Directory
+//
+// List all files and directories present in the current directory or the given
+// path.
+
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.testing.allocator);
+    defer std.process.argsFree(std.testing.allocator, args);
+    const dir = try std.fs.cwd().openDir(if (args.len < 2) "." else args[1], .{ .iterate = true });
+    var dir_iterator = dir.iterate();
+
+    // Iterate Over the Path's
+    while (try dir_iterator.next()) |path| {
+        try stdout.print("{s}\n", .{path.name});
+    }
+}
